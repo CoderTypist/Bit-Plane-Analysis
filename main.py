ca3e434fe7c28b873b1e966d2f17a0ae7079a94d
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2021-12-29 18:19:13 -0600

    Analyze image file

---
+++ main.py
@@ -0,0 +1,12 @@
+import bitplanes as bp
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+def main():
+    bp.analyze('pikachu.png')
+
+    
+if __name__ == '__main__':
+    main()
