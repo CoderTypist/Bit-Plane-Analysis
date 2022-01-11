204c25930711844efbf0d12120c135c9434376a4
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2022-01-11 00:13:37 -0600

    Block avg available instead of moving avg
    Block averages are now available. They are significantly faster than moving averages. The next step is to dynamically determine block sizes so that blocks appear nicely on figures.

--- bitplanes.py
+++ bitplanes.py
@@ -1,7 +1,8 @@
+from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 
 class g:
@@ -15,6 +16,17 @@ class g:
     
     SMALL_WINDOW_SIZE = 100
     LARGE_WINDOW_SIZE = 1000
+    
+    SMALL_MOVING = True
+    LARGE_MOVING = True
+
+
+class Block:
+    def __init__(self, mean, std, start, size):
+        self.mean = mean
+        self.std = std
+        self.start = start
+        self.size = size
 
 
 def get_red(pix: List[Tuple[int,int,int]]) -> np.ndarray:
@@ -46,9 +58,9 @@ def get_color_bit_planes(color: np.ndarray) -> List[List[np.uint8]]:
     return bit_planes
 
 
-def get_bit_plane_percentages(bit_planes: List[List[np.uint8]], window_size=100) -> List[float]:
-    
-    bit_plane_percentages = [ [] for i in range(8) ]
+def get_bit_plane_moving_averages(bit_planes: List[List[np.uint8]], window_size) -> List[float]:
+
+    bit_plane_averages = [ [] for i in range(8) ]
     
     if g.VERBOSE:
         if 0 == window_size:
@@ -66,9 +78,9 @@ def get_bit_plane_percentages(bit_planes: List[List[np.uint8]], window_size=100)
         for i_val in np.arange(len(bp)):
             bp_sum += bp[i_val]
             
-            # cumulative percentages
+            # cumulative averages
             if 0 == window_size:
-                bit_plane_percentages[i_bp].append(bp_sum/(i_val+1))
+                bit_plane_averages[i_bp].append(bp_sum/(i_val+1))
             
             # percentage within the window
             else:
@@ -78,24 +90,73 @@ def get_bit_plane_percentages(bit_planes: List[List[np.uint8]], window_size=100)
                     i_start = i_val + 1
                     
                 i_end = i_val + 2
-                current_window_size = (i_end-1) - i_start
-                bit_plane_percentages[i_bp].append(np.sum(bp[i_start:i_end])/current_window_size)
+                current_window_size = i_end - i_start
+                bit_plane_averages[i_bp].append(np.sum(bp[i_start:i_end])/current_window_size)
                 
     print()
-    return bit_plane_percentages
+    return bit_plane_averages
+
+
+def get_bit_plane_block_percentiles(bit_planes: List[List[np.uint8]], blk_size, bottom=25, middle=50, top=75) -> List[List[Block]]:
     
+    bit_plane_block_percentiles: List[List[Block]] = [ [] for i in range(8) ]
     
-def color_percentages(color_vals, color_name, base_name=None) -> None:
+    if g.VERBOSE:
+        if 0 == blk_size:
+            print('\t- cumulative:')
+        else:
+            print(f'\t- block size {blk_size}:')
+        print('\t\t- ', end='')
+            
+    for i_bp in np.arange(len(bit_planes)):
+        
+        if g.VERBOSE: print(f'{i_bp} ', end='')
+        bp = bit_planes[i_bp]
+        len_bp = len(bp)
+        
+        for i_start in np.arange(0, len_bp, blk_size):
+            
+            to_end = len_bp - i_start
+            
+            i_end = 0
+            cur_blk_size = 0
+            
+            if to_end < blk_size:
+                i_end = len_bp
+                cur_blk_size = to_end
+            else:
+                i_end = i_start + blk_size
+                cur_blk_size = blk_size
+                
+            blk = bp[i_start:i_end]
+            mean = np.mean(blk)
+            std = np.std(blk)
+            bit_plane_block_percentiles[i_bp].append(Block(mean, std, i_start, cur_blk_size))
+            
+    return bit_plane_block_percentiles
+
+    
+def color_averages(color_vals, color_name, base_name=None) -> None:
 
     bit_planes = get_color_bit_planes(color_vals)
     
-    bit_plane_small_window_percentages = None
-    bit_plane_large_window_percentages = None
-    bit_plane_cumulative_percentages = None
+    bit_plane_small_window_averages = None
+    bit_plane_large_window_averages = None
+    bit_plane_cumulative_averages = None
     
-    if g.SMALL: bit_plane_small_window_percentages = get_bit_plane_percentages(bit_planes, window_size=g.SMALL_WINDOW_SIZE)
-    if g.LARGE: bit_plane_large_window_percentages = get_bit_plane_percentages(bit_planes, window_size=g.LARGE_WINDOW_SIZE)
-    if g.CUMULATIVE: bit_plane_cumulative_percentages = get_bit_plane_percentages(bit_planes, window_size=0)
+    # only used for block averages
+    bit_plane_small_std = None
+    bit_plane_large_std = None
+    
+    if g.SMALL:
+        if g.SMALL_MOVING: bit_plane_small_window_averages = get_bit_plane_moving_averages(bit_planes, g.SMALL_WINDOW_SIZE)
+        else: bit_plane_small_window_averages = get_bit_plane_block_percentiles(bit_planes, g.SMALL_WINDOW_SIZE)
+        
+    if g.LARGE: 
+        if g.LARGE_MOVING: bit_plane_large_window_averages = get_bit_plane_moving_averages(bit_planes, g.LARGE_WINDOW_SIZE)
+        else: bit_plane_large_window_averages = get_bit_plane_block_percentiles(bit_planes, g.LARGE_WINDOW_SIZE)
+        
+    if g.CUMULATIVE: bit_plane_cumulative_averages = get_bit_plane_moving_averages(bit_planes, 0)
     
     fig, axs = plt.subplots(4,2, figsize=(15, 10), constrained_layout=True)
     
@@ -115,9 +176,40 @@ def color_percentages(color_vals, color_name, base_name=None) -> None:
     for i in np.arange(8):
         r = i % 4
         c = i // 4
-        if g.SMALL: axs[r,c].plot(bit_plane_small_window_percentages[i], color='gray', alpha=small_window_alpha)
-        if g.LARGE: axs[r,c].plot(bit_plane_large_window_percentages[i], color='gray', alpha=large_window_alpha)
-        if g.CUMULATIVE: axs[r,c].plot(bit_plane_cumulative_percentages[i], color=color_name)
+        
+        if g.LARGE:
+            if g.LARGE_MOVING:
+                axs[r,c].plot(bit_plane_large_window_averages[i], color='gray', alpha=large_window_alpha)
+            else:
+                for blk in bit_plane_large_window_averages[i]:  
+                    
+                    rect = Rectangle((blk.start, blk.mean-(blk.std/2)), 
+                                     blk.size, blk.std, 
+                                     facecolor='gray', edgecolor='black',
+                                     alpha=large_window_alpha)
+                    axs[r,c].add_patch(rect)
+                    
+                    axs[r,c].plot([blk.start, blk.start+blk.size],
+                                  [blk.mean, blk.mean],
+                                  linewidth=3, color='cyan')
+        
+        if g.SMALL: 
+            if g.SMALL_MOVING:
+                axs[r,c].plot(bit_plane_small_window_averages[i], color='gray', alpha=small_window_alpha)
+            else:
+                for blk in bit_plane_small_window_averages[i]:  
+                    
+                    rect = Rectangle((blk.start, blk.mean-(blk.std/2)), 
+                                     blk.size, blk.std, 
+                                     facecolor='gray', edgecolor='black',
+                                     alpha=small_window_alpha)
+                    axs[r,c].add_patch(rect)
+                    
+                    axs[r,c].plot([blk.start, blk.start+blk.size],
+                                  [blk.mean, blk.mean],
+                                  linewidth=3, color='orange')
+        
+        if g.CUMULATIVE: axs[r,c].plot(bit_plane_cumulative_averages[i], color=color_name)
         axs[r,c].set_title(f'bit plane {i}', fontsize=axs_fontsize)
         axs[r,c].set_xlabel('Number of Bytes')
         axs[r,c].set_ylabel('Average')
@@ -135,19 +227,23 @@ def color_percentages(color_vals, color_name, base_name=None) -> None:
     plt.show()
 
 
-def pix_percentages(pix, base_name=None):
+def pix_averages(pix, base_name=None):
 
     if g.VERBOSE: print('RED:')
-    color_percentages(get_red(pix), 'Red', base_name=base_name)
+    color_averages(get_red(pix), 'Red', base_name=base_name)
     
     if g.VERBOSE: print('GREEN:')
-    color_percentages(get_green(pix), 'Green', base_name=base_name)
+    color_averages(get_green(pix), 'Green', base_name=base_name)
     
     if g.VERBOSE: print('BLUE:')
-    color_percentages(get_blue(pix), 'Blue', base_name=base_name)
+    color_averages(get_blue(pix), 'Blue', base_name=base_name)
 
 
-def set_config(save=True, verbose=True,small=True, large=True, cumulative=True, small_window_size=100, large_window_size=1000):
+def set_config(save=True, verbose=True,
+               small=True, large=True, cumulative=True,
+               small_window_size=100, large_window_size=1000,
+               small_moving=True, large_moving=True):
+    
      g.SAVE = save
      g.VERBOSE = verbose
      g.SMALL = small
@@ -155,9 +251,14 @@ def set_config(save=True, verbose=True,small=True, large=True, cumulative=True, 
      g.CUMULATIVE = cumulative
      g.SMALL_WINDOW_SIZE = small_window_size
      g.LARGE_WINDOW_SIZE = large_window_size
+     g.SMALL_MOVING = small_moving
+     g.LARGE_MOVING = large_moving
         
     
-def analyze(fname, save=True, verbose=True, small=True, large=True, cumulative=True, small_window_size=100, large_window_size=1000):
+def analyze(fname, save=True, verbose=True,
+            small=True, large=True, cumulative=True,
+            small_window_size=100, large_window_size=1000,
+            small_moving=True, large_moving=True):
     
     set_config(save=save,
                verbose=verbose,
@@ -165,10 +266,12 @@ def analyze(fname, save=True, verbose=True, small=True, large=True, cumulative=T
                large=large,
                cumulative=cumulative,
                small_window_size=small_window_size,
-               large_window_size=large_window_size)
+               large_window_size=large_window_size,
+               small_moving=small_moving,
+               large_moving=large_moving)
     
     base_name = fname.split('.')[0]
     
     im = Image.open(fname)
     pix = list(im.getdata())
-    pix_percentages(pix, base_name=base_name)
+    pix_averages(pix, base_name=base_name)
